b4c838a0f30a869b3a65f8718b75dffe8393ebf0
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-08-24 01:29:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const colorSelect = document.getElementById('colorSelect');
+// console.log(temp)
+document.getElementsByTagName('input')[0].addEventListener('click', ()=>{
+    // console.log();
+    colorSelect.remove(colorSelect.selectedIndex);
+})
